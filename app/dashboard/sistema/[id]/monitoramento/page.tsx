d0200140b120adedecf5d6bb2da6633b2f717c2d
Carlos Clementino <carlossantiagoclementino36@gmail.com>
2026-03-22 16:03:51 -0300

    Update page.tsx

--- app/dashboard/sistema/[id]/monitoramento/page.tsx
+++ app/dashboard/sistema/[id]/monitoramento/page.tsx
@@ -49,7 +49,7 @@ export default function MonitoramentoPage({ params }: PageProps) {
   const [currentConsumption, setCurrentConsumption] = useState(0)
   const mqttClientRef = useRef<MqttClient | null>(null)
 
-  const BROKER_URL = 'wss://solarmetrics.brazilsouth.cloudapp.azure.com:8084/mqtt'
+  const BROKER_URL = 'ws://solarmetrics.brazilsouth.cloudapp.azure.com:8083/mqtt'
   const PASSWORD = 'solarmetrics'
 
   const connectMQTT = useCallback(async () => {
